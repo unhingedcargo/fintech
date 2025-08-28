81549c49034f08d0e2b13fad7ff7ea56ffb69263
unhingedcargo <unhingedcargo@gmail.com>
2025-08-28 18:45:19 +0530

    update

--- app/api.py
+++ app/api.py
@@ -71,7 +71,9 @@ def delete_contact(request, slug:str):
 @api.patch("/contact/update/{slug}", response=ContactSchema)
 def update_contact(request, slug:str, payload:ContactSchema):
     contact = get_object_or_404(Contact, Q(display_name__icontains=slug) | Q(name__icontains=slug) | Q(company_name__icontains=slug))
-    update_data = payload.dict(exclude_unset=True)
+    # update_data = payload.dict(exclude_unset=True)
+    update_data = payload.dict(exclude_unset=True, exclude_defaults=True, exclude_none=True)
+
 
     for attr, value in update_data.items():
         setattr(contact, attr, value)
